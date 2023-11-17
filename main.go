474c53ae54b5b347f932c4fd704d9c699263f110
Dean Lofts <dean@deanlofts.xyz>
2023-11-17 16:37:07 +1100

    make gooder

--- main.go
+++ main.go
@@ -1,41 +1,98 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 
-	// Replace with the correct import path for your services package
 	"echo-playground/services"
 )
 
 func main() {
-	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
 		panic("Error loading .env file")
 	}
 
-	// Create a new instance of Echo
 	e := echo.New()
 
-	// Define routes
 	e.GET("/", handleRoot)
+	e.GET("/students/flagged", handleFlaggedStudents)
 
-	// Start the server on port 1323
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-// handleRoot is the route handler for the root endpoint
 func handleRoot(c echo.Context) error {
-	// Send a prompt to OpenAI and get the response
 	response, err := services.ChatWithOpenAI("G'day, OpenAI!")
 	if err != nil {
-		// If there's an error, return a 500 internal server error response
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
-	// Return the response from OpenAI
 	return c.String(http.StatusOK, response)
+}
+
+func handleFlaggedStudents(c echo.Context) error {
+	const highAbsenceThreshold = 20
+
+	fmt.Println("Opening CSV file...")
+	f, err := os.Open("data/data.csv")
+	if err != nil {
+		fmt.Printf("Failed to open data file: %v\n", err)
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Failed to open data file: %v", err))
+	}
+	defer f.Close()
+
+	fmt.Println("Reading CSV file...")
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		fmt.Printf("Failed to read data file: %v\n", err)
+		return c.String(http.StatusInternalServerError, fmt.Sprintf("Failed to read data file: %v", err))
+	}
+
+	var flaggedStudents []string
+	var responseMessage string
+	needResponse := true
+
+	for i, record := range records {
+		if i == 0 { // Skip header line
+			continue
+		}
+
+		studentID := record[0]
+		absenceCount, _ := strconv.Atoi(record[3])
+
+		if absenceCount > highAbsenceThreshold {
+			flaggedStudents = append(flaggedStudents, studentID)
+
+			// Generate message only once
+			if needResponse {
+				prompt := "Create a message indicating that a student has been absent too many times and may need additional support."
+				fmt.Println(prompt)
+				response, err := services.ChatWithOpenAI(prompt)
+				if err != nil {
+					fmt.Printf("Error generating message: %v\n", err)
+					return c.String(http.StatusInternalServerError, fmt.Sprintf("Error generating message: %v", err))
+				}
+				responseMessage = response
+				needResponse = false
+			}
+		}
+	}
+
+	if len(flaggedStudents) == 0 {
+		fmt.Println("No flagged students found.")
+		return c.JSON(http.StatusOK, "No flagged students found.")
+	}
+
+	fmt.Println("Returning flagged students with response message...")
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"students": flaggedStudents,
+		"message":  responseMessage,
+	})
 }
--- services/openai_service.go
+++ services/openai_service.go
@@ -1,28 +1,29 @@
+// services/openai_service.go
 package services
 
 import (
 	"context"
 	"os"
+	"time"
 
 	openai "github.com/sashabaranov/go-openai"
 )
 
-// ChatWithOpenAI sends a prompt to ChatGPT and returns the response
 func ChatWithOpenAI(prompt string) (string, error) {
 	client := openai.NewClient(os.Getenv("OPENAI_KEY"))
 
-	resp, err := client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo, // Choose the model you want to use
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: prompt,
-				},
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second) // Increased to 60 seconds
+	defer cancel()
+
+	resp, err := client.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
+		Model: openai.GPT3Dot5Turbo,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: prompt,
 			},
 		},
-	)
+	})
 
 	if err != nil {
 		return "", err
